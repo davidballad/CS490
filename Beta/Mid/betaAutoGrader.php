ff77573fdca2864e87ee9d4bf1d5650330e46991
moostack <jwilso414@gmail.com>
2019-12-20 16:08:10 -0500

    Name added

--- Beta/Mid/betaAutoGrader.php
+++ Beta/Mid/betaAutoGrader.php
@@ -1,4 +1,7 @@
 <?php
+
+//Jacquelynn Wilson
+//Auto Grader - Section 003 - Group 4 
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate"); // HTTP/1.1
 header("Cache-Control: post-check=0, pre-check=0", false);
